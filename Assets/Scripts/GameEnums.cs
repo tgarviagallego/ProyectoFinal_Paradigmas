53fe5717bc3a81d3e60ae355593563a1dcd1c432
tgarviagallego <202105183@alu.comillas.edu>
2025-01-07 00:08:08 +0100

    Advancements in GameManager and DataManager instantiations and basic functions.

---
+++ Assets/Scripts/GameEnums.cs
@@ -0,0 +1,36 @@
+namespace SpellboundForest.Enums
+{
+    public enum GameState
+    {
+        MainMenu,
+        Playing,
+        Paused,
+        GameOver,
+        Victory
+    }
+
+    public enum SpellType
+    {
+        Incendium,
+        PetrificusTotalus,
+        AvadaKedavra
+
+    }
+
+    public enum MonsterType
+    {
+        Dwarf,
+        Werewolf,
+        Spirit
+    }
+
+    public enum CharacterState
+    {
+        Idle,
+        Moving,
+        Attacking,
+        Jumping,
+        Dead,
+        Victorious
+    }
+}
---
+++ Assets/Scripts/ManagersLayer/SpawnManager.cs
@@ -0,0 +1,47 @@
+using SpellboundForest.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private static SpawnManager _instance;
+    public static SpawnManager Instance => _instance;
+
+    private SpellFactory spellFactory = FindObjectOfType<SpellFactory>();
+    private MonsterFactory monsterFactory = FindObjectOfType<MonsterFactory>();
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = null;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SpawnMonster(MonsterType type, Vector3 position)
+    {
+        monsterFactory.CreateMonster(type, position);
+    }
+
+    internal void InitializeSpawnPoints()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void SpawnMultiplayerWizards()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void SpawnSinglePlayerWizard()
+    {
+        throw new NotImplementedException();
+    }
+}
