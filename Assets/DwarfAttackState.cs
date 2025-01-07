6bdf7919b8ae99614e61f59271690d7625a37672
TereFranco <112712182+TereFranco@users.noreply.github.com>
2025-01-07 20:17:16 +0100

    Started swarf AI and statemachine to control animations

---
+++ Assets/DwarfAttackState.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DwarfAttackState : StateMachineBehaviour
+{
+    NavMeshAgent agent;
+    Transform player;
+
+    public float stopAttackingDistance = 2.5f;
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Wizard").transform;
+        agent = animator.GetComponent<NavMeshAgent>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        LookAtPlayer(); //diff to built in LookAt, because it is attacking 
+
+        // chack if agent hast to stop the attack
+        float distanceFromPlayer = Vector3.Distance(player.position, animator.transform.position);
+        if (distanceFromPlayer < stopAttackingDistance)
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void LookAtPlayer()
+    {
+        Vector3 direction = player.position - agent.transform.position;
+        agent.transform.rotation = Quaternion.LookRotation(direction);
+
+        var yRotation = agent.transform.eulerAngles.y;
+        agent.transform.rotation = Quaternion.Euler(0, yRotation, 0); // if for example thep kayer is higher not to rotate
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+}
---
+++ Assets/DwarfWalkState.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEditor.Experimental.GraphView;
+
+public class DwarfWalkState : StateMachineBehaviour
+{
+    float timer;
+    public float walkingTime = 10f; //how much animal is still
+
+
+    Transform player; // to know if player is close to us
+    NavMeshAgent agent; // the ai we are going to use
+
+    public float detectionAreaRadius = 18f;
+    public float walkSpeed = 2f;
+
+    List<Transform> waypointsList = new List<Transform>();// positions around the bear to know where to go
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // initialize
+        player = GameObject.FindGameObjectWithTag("Wizard").transform;
+        agent = animator.GetComponent<NavMeshAgent>();
+
+
+        agent.speed = walkSpeed;
+        timer = 0;
+
+        // get waypoints and move to the first one
+        GameObject waypointsCluster = animator.GetComponent<MonsterWaypoints>().monsterWaypointsCluster;
+        foreach (Transform t in waypointsCluster.transform)
+        {
+            waypointsList.Add(t);
+        }
+
+        Vector3 firstPosition = waypointsList[Random.Range(0,waypointsList.Count)].position;
+        agent.SetDestination(firstPosition);
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // if arrived at one waypoint, move to other
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            agent.SetDestination(waypointsList[Random.Range(0, waypointsList.Count)].position);
+        }
+
+        // idle
+        timer += Time.deltaTime;
+        if (timer > walkingTime) 
+        {
+            animator.SetBool("isWalking", false);
+
+        }
+
+        // chase state 
+        float distanceFromPlayer = Vector3.Distance(player.position, animator.transform.position);
+        if (distanceFromPlayer < detectionAreaRadius)
+        {
+            animator.SetBool("isChasing", true);
+        }
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(agent.transform.position);
+    }
+
+
+}
