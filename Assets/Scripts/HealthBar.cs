b32b831a6d3677373ed7cfdf0f0b0ecbc0eeb423
TereFranco <112712182+TereFranco@users.noreply.github.com>
2025-01-08 23:36:02 +0100

    solved problem on life bar text

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class HealthBar : MonoBehaviour
 {
     private Slider slider;
 
-    public Text healthCounter;
+    public TMP_Text healthCounter;
 
     public GameObject playerState;
 
