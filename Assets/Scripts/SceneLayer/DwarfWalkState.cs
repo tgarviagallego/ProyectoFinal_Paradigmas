fc292b77bfd67e977595ff26371f04b42551fbec
tgarviagallego <202105183@alu.comillas.edu>
2025-01-11 23:27:24 +0100

    Changes to generate SpellboundForest.exe

--- Assets/Scripts/SceneLayer/DwarfWalkState.cs
+++ Assets/Scripts/SceneLayer/DwarfWalkState.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
-using UnityEditor.Experimental.GraphView;
 
 public class DwarfWalkState : StateMachineBehaviour
 {
